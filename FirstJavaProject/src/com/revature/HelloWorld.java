76a8b102ea64bec1fd08fa9921f050730110ddbd
John Pham <johnpham739@gmail.com>
2021-08-23 14:36:40 -0500

    add HelloWorld class

---
+++ FirstJavaProject/src/com/revature/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.revature;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		//syso + ctrl + space
+		System.out.println("Hello World...from the IDE");
+		
+	}
+
+}
